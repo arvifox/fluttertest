062af9ebe33ee36b6042ef9ce5b78e3ba690d25a
Arvi Fox <arvifox@hotmail.com>
2019-01-14 15:30:34 +0300

    handle incoming intent

--- android/app/src/main/kotlin/com/arvifox/fluttertest/MainActivity.kt
+++ android/app/src/main/kotlin/com/arvifox/fluttertest/MainActivity.kt
@@ -4,10 +4,42 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.content.Intent
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+import java.nio.ByteBuffer;
+
+import io.flutter.plugin.common.ActivityLifecycleListener;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+
+class MainActivity : FlutterActivity() {
+
+    private var sharedText: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        val intent = intent
+        val action = intent.action
+        val type = intent.type
+
+        if (Intent.ACTION_SEND == action && type != null) {
+            if ("text/plain" == type) {
+                handleSendText(intent) // Handle text being sent
+            }
+        }
+
+        MethodChannel(flutterView, "app.channel.shared.data").setMethodCallHandler { call, result ->
+            if (call.method.contentEquals("getSharedText")) {
+                result.success(sharedText)
+                sharedText = null
+            }
+        }
+    }
+
+    fun handleSendText(intent: Intent) {
+        sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+    }
 }
