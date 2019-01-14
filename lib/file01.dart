1e905a7d9327652a473aa16727d888c5b9dfda8b
Arvi Fox <arvifox@hotmail.com>
2019-01-14 12:57:02 +0300

    english word

--- lib/file01.dart
+++ lib/file01.dart
@@ -5,7 +5,6 @@ import 'package:english_words/english_words.dart';
 class MyApp23 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -13,9 +12,22 @@ class MyApp23 extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
