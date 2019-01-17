ac512e10d1a7b7c05883722cdac417b20867e63b
Arvi Fox <arvifox@hotmail.com>
2019-01-17 15:43:31 +0300

    splash doesnt work

--- lib/advert/splash.dart
+++ lib/advert/splash.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import '../nav_drawer.dart';
+
+import 'dart:async';
 
 class AdvertSplash extends StatelessWidget {
   @override
@@ -9,7 +13,39 @@ class AdvertSplash extends StatelessWidget {
         horizontal: 20.0,
       ),
       color: Color(0xFFFFAA88),
-      child: Image.asset("resources/images/splash_image.png"),
+      child: _SplashImage(),
     );
+  }
+}
+
+class _SplashImage extends StatefulWidget {
+  _SplashImage({Key key}) : super(key: key);
+
+  @override
+  _SplashImageState createState() => _SplashImageState();
+}
+
+class _SplashImageState extends State<_SplashImage> {
+
+  _afterSplash() {
+    Route route = MaterialPageRoute(builder: (context) => MyAppDrawer());
+    Navigator.of(context).pushReplacement(route);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("resources/images/splash_image.png");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData(_afterSplash());
+  }
+
+  void _loadData(after()) async {
+    await Timer(Duration(seconds: 3), () {
+      after();
+    });
   }
 }
