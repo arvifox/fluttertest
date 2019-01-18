bdba8ce8a0c7c241cd549cddcce31bbcc930d563
Arvi Fox <arvifox@hotmail.com>
2019-01-18 15:53:26 +0300

    http request, decode russian language utf8

--- lib/advert/advert_list.dart
+++ lib/advert/advert_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:xml/xml.dart' as xml;
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AdvertList extends StatefulWidget {
   AdvertList({Key key}) : super(key: key);
@@ -28,7 +29,7 @@ class _AdvertListState extends State<AdvertList> {
     http.Response response = await http.get(
         "https://www.hibiny.com/iphone/board.php?articles_per_page=10&from=0&full=1");
     if (response.statusCode == 200) {
-      xml.XmlDocument body = xml.parse(response.body);
+      xml.XmlDocument body = xml.parse(utf8.decode(response.bodyBytes));
       var d = body.findAllElements("item");
       var f =
           d.map((no) => _AnAdvert.title(no.findElements("title").single.text));
@@ -43,12 +44,13 @@ class _AdvertListState extends State<AdvertList> {
       for (var i = 0; i < ads.length; i++) {
         ads_widget.add(_getRow(i));
       }
+      ads_widget = List.from(ads_widget);
     });
   }
 
   Widget _getRow(int i) {
     return GestureDetector(
-      child: Padding(padding: EdgeInsets.all(10.0), child: Text(ads[i].title)),
+      child: Padding(padding: EdgeInsets.all(2.0), child: Text(ads[i].title)),
       onTap: () {
         print("print");
       },
