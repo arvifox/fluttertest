959a8a425bab2346c7a5dd59fd53bd08ca8f0379
Arvi Fox <arvifox@hotmail.com>
2019-02-07 16:46:53 +0300

    battery level, method channel, platform code

--- android/app/src/main/kotlin/com/arvifox/fluttertest/MainActivity.kt
+++ android/app/src/main/kotlin/com/arvifox/fluttertest/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.arvifox.fluttertest
 
 import android.os.Bundle
-
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
-import android.content.Intent
 
 import java.nio.ByteBuffer;
 
@@ -37,9 +42,36 @@ class MainActivity : FlutterActivity() {
                 sharedText = null
             }
         }
+
+        MethodChannel(flutterView, "com.arvifox.fluttertest/battery").setMethodCallHandler { methodCall, result ->
+            if (methodCall.method == "getBatteryLevel") {
+                val batteryLevel = getBatteryLevel()
+
+                if (batteryLevel != -1) {
+                    result.success(batteryLevel)
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
     }
 
     fun handleSendText(intent: Intent) {
         sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+    }
+
+    private fun getBatteryLevel(): Int {
+        val batteryLevel: Int
+        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+        } else {
+            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+        }
+
+        return batteryLevel
     }
 }
